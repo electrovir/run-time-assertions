83a3e9526e501b363994cbb6c0070dc482f27461
electrovir <electrovir@users.noreply.github.com>
2024-01-02 15:52:51 +0000

    [minor] export new functions

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from './assert-throws';
 export * from './assert-type-of';
 export * from './assertion.error';
 export * from './assertions';
+export * from './diff';
+export * from './is-primitive';
 export * from './run-time-types';
